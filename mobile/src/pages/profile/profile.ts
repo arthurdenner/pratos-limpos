701dd775335ddcaf4149dd4e47608e74b28ceecc
arthurdenner <arthurdenner7@gmail.com>
2018-06-04 23:54:18 -0300

    chore(profile): Showing user's information and starting the edit of the user's school

--- mobile/src/pages/profile/profile.ts
+++ mobile/src/pages/profile/profile.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { Http, Response } from '@angular/http';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { SelectSearchableComponent } from 'ionic-select-searchable';
 import { Storage } from '@ionic/storage';
 import { AngularFireAuth } from 'angularfire2/auth';
+import { AngularFireDatabase } from 'angularfire2/database';
 import { LoginPage } from '../login/login';
-import { APP_KEY } from '../../app/constants';
+import { APP_KEY, API_URL } from '../../app/constants';
 
 @IonicPage()
 @Component({
@@ -11,43 +14,64 @@ import { APP_KEY } from '../../app/constants';
   templateUrl: 'profile.html',
 })
 export class ProfilePage {
-  public usuario = {};
+  public loggedUser = {};
+  public schools: any = [];
 
   constructor(
+    public http: Http,
     public navCtrl: NavController,
     public navParams: NavParams,
     public storage: Storage,
-    public firebaseAuth: AngularFireAuth
+    public firebaseAuth: AngularFireAuth,
+    public db: AngularFireDatabase
   ) {}
 
   ionViewDidLoad() {
     this.storage
       .get(APP_KEY)
       .then(storageData => {
-        this.usuario = storageData;
+        const userSchoolRef = this.db.object(`schools/${storageData.idSchool}`);
+
+        userSchoolRef.valueChanges().subscribe(school => {
+          this.schools = [school];
+          this.loggedUser = {
+            ...storageData,
+            school,
+          };
+        });
       })
-      .catch(err => {
-        console.error(err);
-        alert('An error occured reading from the storage!');
-      });
+      .catch(err => alert('An error occured reading from the storage!'));
   }
 
-  selectSchool() {
-    console.log('TODO: Escolher escola!');
+  searchSchools(event: { component: SelectSearchableComponent; text: string }) {
+    const text = (event.text || '').trim().toLowerCase();
+    console.log('text', text);
+
+    if (!text) {
+      event.component.items = [];
+      return;
+    } else if (event.text.length < 3) {
+      return;
+    }
+
+    event.component.isSearching = true;
+
+    this.http
+      .get(`${API_URL}/search-schools/?name=${text}`)
+      .subscribe((response: Response) => {
+        console.log(response.json())
+        this.schools = [];
+        event.component.items = response.json();
+        event.component.isSearching = false;
+      });
   }
 
   signOut() {
     this.firebaseAuth.auth.signOut().then(() => {
       this.storage
         .set(APP_KEY, {})
-        .then(() => {
-          console.log('Signed out successfully!');
-          this.navCtrl.setRoot(LoginPage);
-        })
-        .catch(err => {
-          console.error(err);
-          alert('An error occured trying to login!');
-        });
+        .then(() => this.navCtrl.setRoot(LoginPage))
+        .catch(err => alert('An error occured trying to sign out!'));
     });
   }
 }
