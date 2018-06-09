13f6539eb280b9655a13cc4659f6ec2fc3b742ac
arthurdenner <arthurdenner7@gmail.com>
2018-06-09 20:12:40 -0300

    feat(evaluation): Saving the user's evaluation on Firebase

--- mobile/src/pages/evaluation/evaluation.ts
+++ mobile/src/pages/evaluation/evaluation.ts
@@ -1,6 +1,11 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { Storage } from '@ionic/storage';
+import { AngularFireDatabase } from 'angularfire2/database';
+import format from 'date-fns/format';
 import { TabsService } from '../../services/tabs';
+import { HomePage } from '../home/home';
+import { APP_KEY } from '../../app/constants';
 
 @IonicPage()
 @Component({
@@ -17,23 +22,52 @@ export class EvaluationPage {
     qualidadeDaMerendaRecebida: null,
     nivelDeSatisfacao: null,
   };
+  private idSchool: string = null;
+  private idUser: string = null;
 
   constructor(
+    private db: AngularFireDatabase,
     public navCtrl: NavController,
     public navParams: NavParams,
+    private storage: Storage,
     private tabs: TabsService
   ) {}
 
   ionViewDidLoad() {
     this.tabs.hide();
+
+    this.storage
+      .get(APP_KEY)
+      .then(storageData => {
+        this.idSchool = storageData.idSchool;
+        this.idUser = storageData.uid;
+      })
+      .catch(err => alert('An error occured reading from the storage!'));
   }
 
   ionViewWillLeave() {
     this.tabs.show();
   }
 
-  logForm() {
-    console.log(this.evaluation);
+  saveEvaluation() {
+    const today = format(new Date(), 'YYYY-MM-DD');
+    const evaluationsRef = this.db.list('/evaluations');
+
+    evaluationsRef
+      .push({
+        date: today,
+        evaluation: this.evaluation,
+        idSchool: this.idSchool,
+        idUser: this.idUser,
+        idUser_date: `${this.idUser}_${today}`,
+      })
+      .then(
+        () => {
+          alert('Avaliação enviada com sucesso! Obrigado!');
+          this.navCtrl.setRoot(HomePage);
+        },
+        err => alert('Houve um erro na hora de salvar a avaliação.')
+      );
   }
 
   onChangeTeveMerenda() {
