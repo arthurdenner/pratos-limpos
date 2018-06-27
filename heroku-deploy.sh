9e8fb284bf1b94998e99f4c30311dcc04b0c6c53
Neftali Júnior <junior_cavalcante-al@hotmail.com>
2018-06-26 23:52:04 -0300

    build(pwa): Updating heroku-deploy.sh

--- heroku-deploy.sh
+++ heroku-deploy.sh
@@ -1,5 +1,7 @@
 git remote add heroku https://git.heroku.com/pratos-limpos.git
+git checkout heroku
+git merge heroku master
 git add .
 git commit -am 'Updating Heroku app'
-git subtree push --prefix mobile heroku master
+git push heroku `git subtree split --prefix mobile master`:master --force
 heroku open
